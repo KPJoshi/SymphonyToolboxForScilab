4556826501baf23a72f72ed448f1fccbf5a1a4e1
Keyur Joshi <keyurpjoshi@gmail.com>
2015-06-05 15:51:24 +0530

    Made the MPS loader more robust and secure

--- sci_sym_load_mps.cpp
+++ sci_sym_load_mps.cpp
@@ -3,13 +3,13 @@
  * template.cpp
  * contains function for loading .mps file to symphony
  * By Iswarya
- * Last edit on 20-05-2015 by Iswarya 
  */
-
 #include <symphony.h>
+#include "sci_iofunc.hpp"
+
 extern sym_environment* global_sym_env;//defined in globals.cpp
 
-extern "C" {
+extern "C"{
 #include <api_scilab.h>
 #include <Scierror.h>
 #include <BOOL.h>
@@ -18,9 +18,11 @@ extern "C" {
 
 //This function is for loading a mps file to symphony
 int sci_sym_load_mps(char *fname, unsigned long fname_len){
-	
+
 	// Error management variable
 	SciErr sciErr;
+
+	//data declarations
 	double status=0.0;//assume error status
 	int *piAddressVarOne = NULL;//pointer used to access argument of the function
 	char file[100];//string to hold the name of .mps file
@@ -35,46 +37,39 @@ int sci_sym_load_mps(char *fname, unsigned long fname_len){
 	//check whether there is an error or not.
 	if (sciErr.iErr){
         printError(&sciErr, 0);
-        return 0;
+        return 1;
 	}
-	
-	
+	if ( !isStringType(pvApiCtx,piAddressVarOne) )
+	{
+		Scierror(999,"Wrong type for input argument 1: A file name is expected.\n");
+		return 1;
+	}
+
 	//read the value in that pointer pointing to file name
 	int err=getAllocatedSingleString(pvApiCtx, piAddressVarOne, &ptr);
 
 	//ensure that environment is active
 	if(global_sym_env==NULL){
 		sciprint("Error: Symphony environment not initialized. Please run 'sym_open()' first.\n");
-		}
-	else {
+	}else{
 		output=sym_read_mps(global_sym_env,ptr);//loading .mps file to symphony
-		if(output==FUNCTION_TERMINATED_ABNORMALLY)
+		if(output==FUNCTION_TERMINATED_ABNORMALLY || output==ERROR__READING_MPS_FILE)
 		{
 			status=0.0;//function did not invoke successfully
-			sciprint("Function terminated abnormally,didnot execute\n");
+			sciprint("Error while reading file\n");
 		}
 		else if(output==FUNCTION_TERMINATED_NORMALLY)
 		{
 			status=1.0;//no error in executing the function
-			sciprint("Function executed successfully\n");
+			sciprint("File read successfully\n");
 		}
-		
-		
-		}
-	
-	int e=createScalarDouble(pvApiCtx,nbInputArgument(pvApiCtx)+1,status);
-	if (e){
-		AssignOutputVariable(pvApiCtx, 1) = 0;
-		return 1;
-		}
-
-	AssignOutputVariable(pvApiCtx, 1) = nbInputArgument(pvApiCtx) + 1;
-	ReturnArguments(pvApiCtx);
-
-	return 0;
 	}
 
+	if(returnDoubleToScilab(status))
+		return 1;
 
+	return 0;
+}
 
 }
 
